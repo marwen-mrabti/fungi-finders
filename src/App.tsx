533969861a92bda68c8df4c282ae26c8fc56588d
marwen <marwen.mrabti.me@gmail.com>
2024-12-19 13:06:19 +0100

    chore: prepare assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="bg-background text-foreground grid min-h-dvh w-full max-w-dvw grid-cols-1 overflow-x-clip">
+    <div className="">
       start here
     </div>
   );
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,13 @@
-export { default as ReactLogo } from "./react.svg";
+export { default as bestSeasonsImg } from "./best-seasons.webp";
+export { default as botanicalExpertiseImg } from "./botanical-expertise.webp";
+export { default as culinaryDelightImg } from "./culinary-delight.webp";
+export { default as fungiFindersLogoImg } from "./fungi-finders.svg";
+export { default as getToKnowImg } from "./get-to-know.webp";
+export { default as hamburgerImg } from "./hamburger.svg";
+export { default as heroImg } from "./hero.webp";
+export { default as identifyMushroomsImg } from "./identify-mushrooms.webp";
+export { default as natureImg } from "./nature.webp";
+export { default as outdoorExplorationImg } from "./outdoor-exploration.webp";
+export { default as startForagingImg } from "./start-foraging.webp";
+export { default as whereToFindImg } from "./where-to-find.webp";
+export { default as whereToLookImg } from "./where-to-look.webp";
