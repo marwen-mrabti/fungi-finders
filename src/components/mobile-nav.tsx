6b2927b7a661d64a93fc99de2da9f06a89ecc2d3
marwen <marwen.mrabti.me@gmail.com>
2024-12-22 11:04:28 +0100

    feat: mobile nav menu using popover and anchor api

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -1,31 +1,47 @@
 import { Link } from "@tanstack/react-router";
 import { Menu } from "lucide-react";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { cn } from "../lib/utils";
 import { type T_NavItem } from "./nav";
 
 export default function MobileNav({ navList }: { navList: T_NavItem[] }) {
   const navRef = useRef<HTMLDivElement>(null);
-  const [isOpen, setIsOpen] = useState(false);
+  const navListRef = useRef<HTMLDivElement>(null);
+
+  const closePopover = () => {
+    if (navListRef.current) {
+      navListRef.current.setAttribute("popover-open", "false");
+    }
+  };
 
   useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (navRef.current && !navRef.current?.contains(event.target as Node)) {
-        setIsOpen(false);
+    const handleCloseMenu = (event: Event) => {
+      event.stopPropagation();
+      if (
+        navRef.current &&
+        event.target instanceof HTMLElement &&
+        !navRef.current.contains(event.target)
+      ) {
+        closePopover();
       }
     };
-    document.addEventListener("click", handleClickOutside);
+
+    document.addEventListener("click", handleCloseMenu);
+    document.addEventListener("touchend", handleCloseMenu);
+
     return () => {
-      document.removeEventListener("click", handleClickOutside);
+      document.removeEventListener("click", handleCloseMenu);
+      document.removeEventListener("touchend", handleCloseMenu);
     };
   }, []);
 
   return (
-    <div ref={navRef} className="relative grid lg:hidden">
+    <div ref={navRef} className="grid grid-cols-1 place-items-center lg:hidden">
       <button
         aria-label="Open navigation menu"
         className="group relative cursor-pointer"
-        onClick={() => setIsOpen((prev) => !prev)}
+        popoverTarget="mobile-nav-list"
+        style={{ anchorName: "--mobile-nav-list" } as React.CSSProperties}
       >
         <Menu
           className={cn(
@@ -35,21 +51,28 @@ export default function MobileNav({ navList }: { navList: T_NavItem[] }) {
       </button>
 
       <div
+        ref={navListRef}
+        popover="auto"
+        id="mobile-nav-list"
         className={cn(
-          "bg-background-accent-main min-w-54 rounded-sm px-2 shadow-lg",
-          "absolute top-0 right-0 translate-x-[-3%] translate-y-[20%]",
-          "invisible h-0 transition-all transition-discrete duration-300 ease-linear",
-          {
-            "visible h-auto": isOpen,
-          },
+          "bg-background-accent-light absolute mt-1 mr-2 min-w-54 overflow-clip rounded-sm px-2 shadow-lg",
+          "h-0 opacity-0 open:h-fit open:opacity-100 starting:open:h-0 starting:open:opacity-0",
+          "transition-all transition-discrete duration-500",
+          "[&:popover-open]:bg-background-accent-main",
         )}
+        style={
+          {
+            positionAnchor: "--mobile-nav-list",
+            top: "anchor(--mobile-nav-list bottom)",
+            justifySelf: "anchor-center",
+          } as React.CSSProperties
+        }
       >
         <ul className={cn("divide-main flex flex-col divide-y-1")}>
           {navList.map((item) => (
             <li
               key={item.name}
               className="text-high-contrast hover:text-high-contrast/80 text-md w-full cursor-pointer font-bold"
-              onClick={() => setIsOpen(false)}
             >
               <Link
                 to={item.link}
