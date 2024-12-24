0424a2457dfa18a3920a248b98ecf6b5d98a6121
marwen <marwen.mrabti.me@gmail.com>
2024-12-24 13:11:43 +0100

    ci: remove tanstack dev tools in production env

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,12 +10,15 @@ export const Route = createRootRoute({
 });
 
 function RootComponent() {
+  const nodeEnv = process.env.NODE_ENV;
+  console.log(nodeEnv);
+
   return (
     <div className="bg-background-main text-main relative h-full min-h-dvh w-full max-w-dvw overflow-clip text-base">
       <Header />
       <Outlet /> {/* the main content will render here */}
       <Footer />
-      <TanStackRouterDevtools />
+      {nodeEnv === "development" ? <TanStackRouterDevtools /> : null}
     </div>
   );
 }
